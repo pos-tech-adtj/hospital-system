ab56d56f939d7d9d14d1db769588511a2354b7ae
ArtthurMax <artthurmaximo@uol.com.br>
2026-08-10 22:08:11 -0300

    Schema GraphQL Agendamento/Historico

--- api-agendamento/src/main/java/com/fiap/api_agendamento/repository/AgendamentoRepository.java
+++ api-agendamento/src/main/java/com/fiap/api_agendamento/repository/AgendamentoRepository.java
@@ -2,12 +2,13 @@ package com.fiap.api_agendamento.repository;
 
 import com.fiap.api_agendamento.domain.Agendamento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface AgendamentoRepository
-        extends JpaRepository<Agendamento, UUID> {
+        extends JpaRepository<Agendamento, UUID>, JpaSpecificationExecutor<Agendamento> {
 
         List<Agendamento> findAllByOrderByDataHoraDesc();
 
